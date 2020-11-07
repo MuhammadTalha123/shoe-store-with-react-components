13c250504e8108956eb68c4148b873d71652c3ff
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-07 17:05:20 +0500

    update index.js in components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {HomePage } from "./Home";
+export {LaunchPage } from "./Launch";
+export {LaunchIndexPage } from "./LaunchIndex";
+export {shoes } from "./shoes";
+export {NavElement } from "./NavElement";
+export {LaunchShoePageOne , LaunchShoePageTwo , LaunchShoePageThere } from "./LaunchShoe"
