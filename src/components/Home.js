e947573b720621c87e72af3f18b862440c72b1f5
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-07 17:30:56 +0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ export const HomePage = () =>{
     return (
     <>
     <h1>You are at home Page</h1>
-    <h2>Thank you to visit our shoes store.</h2>
+    <h1>Thank you to visit our shoes store.</h1>
+    <h1>Welcome!</h1>
     </>
         );
 }
