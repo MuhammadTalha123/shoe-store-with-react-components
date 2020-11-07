bc3f7b1aadb694ec49498e969798102f6e585d35
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-07 17:12:39 +0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HomePage, LaunchPage, NavElement, LaunchShoePageOne, LaunchShoePageTwo, LaunchShoePageThere } from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <NavElement />
+      <Switch>
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/Launch" component={LaunchPage} />
+        <Route path="/air-jordan-3-valor-blue" component={LaunchShoePageOne} />
+        <Route path="/jordan-mars-270-london" component={LaunchShoePageTwo} />
+        <Route path="/air-jordan-1-zoom-racer-blue" component={LaunchShoePageThere} />
+      </Switch>
+    </Router>
   );
 }
 
