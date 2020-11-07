fc40bd0fbf2e883533f32798e20970e3fe425dc5
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-07 17:23:16 +0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,9 @@
 import React from "react";
 export const HomePage = () =>{
-    return <h1>You are at home</h1>
+    return (
+    <>
+    <h1>You are at home Page</h1>
+    <h2>Thank you to visit our shoes store.</h2>
+    </>
+        );
 }
